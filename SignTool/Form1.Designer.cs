cdbcc46384cfaf7be5f85d038683497c822b9797
TheBoyLeastLikelyTo <35753008+TheBoyLeastLikelyTo@users.noreply.github.com>
2022-06-27 02:45:55 -0700

    SignTool version 1.0

---
+++ SignTool/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SignTool1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.logtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sign folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // logtxt
+            // 
+            this.logtxt.AcceptsReturn = true;
+            this.logtxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logtxt.Location = new System.Drawing.Point(12, 41);
+            this.logtxt.Multiline = true;
+            this.logtxt.Name = "logtxt";
+            this.logtxt.Size = new System.Drawing.Size(595, 327);
+            this.logtxt.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 380);
+            this.Controls.Add(this.logtxt);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Signtool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox logtxt;
+    }
+}
+
---
+++ SignTool/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignTool1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog open;
+
+            open = new OpenFileDialog();
+            open.Filter = "Executables|*.exe;*.dll";
+            open.Multiselect = true;
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in open.FileNames)
+                {
+                    logtxt.Text += fileName + "...";
+
+                    Application.DoEvents();
+
+                    SignTool.SignWithCert(fileName, "http://timestamp.verisign.com/scripts/timstamp.dll");
+
+                    logtxt.Text += "OK" + Environment.NewLine;
+                }
+
+                System.Windows.Forms.MessageBox.Show("Done");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folderDialog;
+
+            folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string[] exefiles = System.IO.Directory.GetFiles(folderDialog.SelectedPath, "*.exe", System.IO.SearchOption.AllDirectories);
+                string[] dllfiles = System.IO.Directory.GetFiles(folderDialog.SelectedPath, "*.dll", System.IO.SearchOption.AllDirectories);
+
+                foreach (string fileName in exefiles)
+                {
+                    logtxt.Text += fileName + "...";
+                    
+                    Application.DoEvents();
+
+                    SignTool.SignWithCert(fileName, "http://timestamp.verisign.com/scripts/timstamp.dll");
+
+                    logtxt.Text += "OK" + Environment.NewLine;
+                }
+
+                foreach (string fileName in dllfiles)
+                {
+                    logtxt.Text += fileName + "...";
+
+                    Application.DoEvents();
+
+                    SignTool.SignWithCert(fileName, "http://timestamp.verisign.com/scripts/timstamp.dll");
+
+                    logtxt.Text += "OK" + Environment.NewLine;
+                }
+
+                System.Windows.Forms.MessageBox.Show("Done");
+            }
+        }
+    }
+}
